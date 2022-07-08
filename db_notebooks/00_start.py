8e24d20bb345320ae9dedfec3a3ea3e6b7a15c5f
while-zero <while-zero>
2022-07-08 12:38:40 +0000

    add notebook

---
+++ db_notebooks/00_start.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+import sys
+sys.path.append('/Workspace/Repos/db_repo/db/')
+import db_module.utils
+
+# COMMAND ----------
+
+db_module.utils.ut_hello()
+
+# COMMAND ----------
+
+

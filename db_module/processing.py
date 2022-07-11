ea097215b9aee376ac36ae3c110ff889f950c0fd
jnk <57685812+while-zero@users.noreply.github.com>
2022-07-11 17:56:39 +0200

    xyz

--- db_module/processing.py
+++ db_module/processing.py
@@ -1,7 +1,32 @@
 from dataclasses import dataclass
+from typing import Callable, List
 
 
 @dataclass
-class DataClassCard:
-    rank: str
-    suit: str
+class Source:
+    name: str
+    type: str
+    src: str
+@dataclass
+class Activity:
+    name: str
+    source: Source
+    action: Callable[[Source], None]
+
+
+@dataclass
+class Stage:
+    name: str
+    activities: List[Activity]
+@dataclass
+class Process:
+    stages: List[Stage]
+
+@dataclass
+class ProcessList:
+    processes: List[Process]
+
+@dataclass
+class Client:
+    name: str
+    processes: ProcessList
---
+++ db_notebooks/01_testing.py
@@ -0,0 +1,38 @@
+# Databricks notebook source
+import sys
+
+sys.path.append('/Workspace/Repos/db_repo/db/')
+# COMMAND ----------
+import db_module.processing
+import db_module.utils
+
+
+def first_client_function(src: db_module.processing.Source):
+    print(f"source file name: {src.name}")
+    print('xxx')
+
+def second_client_function(src: db_module.processing.Source):
+    print(f"source file name: {src.name}")
+    print('yyy')
+
+
+first_client = db_module.processing.Client('Stefan', 
+                db_module.processing.ProcessList(
+                    [
+                        db_module.processing.Process(
+                            [
+                                db_module.processing.Stage('ingest', 
+                                    [
+                                        db_module.processing.Activity(
+                                                'read_first_file', 
+                                                db_module.processing.Source('iris','csv','https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv'), 
+                                                first_client_function)
+                                    ])
+                            ])
+                    ]
+                )
+            )
+
+
+
+# COMMAND ----------
